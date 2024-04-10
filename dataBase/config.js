5e6a2f09cc16bd59cb703fc60296c63f923d004d
2172015javier <narcisista12@gmail.com>
2024-04-10 15:09:00 -0500

    Fin sección 12 - version 5.0.0

--- dataBase/config.js
+++ dataBase/config.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
+
 const dbConexion = async () => {
   try {
-    await mongoose.connect(
-        process.env.MONGODB_CNN,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      // La opción useNewUrlParser ya no es necesaria y está obsoleta
+      // La opción useUnifiedTopology es ahora la predeterminada y no necesita ser especificada
+    });
 
-    console.log("Base de datos");
+    console.log("Conexión a la base de datos establecida correctamente");
   } catch (error) {
-    console.log(error);
-
-    throw new Error("Error  a la hora de iniciar la base de datos");
+    console.error("Error al conectar a la base de datos:", error.message);
+    throw new Error("Error al iniciar la base de datos");
   }
 };
 
